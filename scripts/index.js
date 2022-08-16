30719f430ac37bf2c0a7b2b80069f1911853ce0b
nicole-drml <doromal.cole@gmail.com>
2022-08-16 19:45:53 +0800

    fixes large video not pushing on github

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-;(() => {
-  const dataName = document.querySelector('[data-name]')
-  const qStr = window.location.search
-  const urlParams = new URLSearchParams(qStr)
-  const name = urlParams.get('name')
-
-  dataName.textContent = `Thank you, ${name}!`
-})()
